ca059fc255ec2ec7b15fdc656cf206eb2e262277
Nghia <nphan@fandm.edu>
2020-03-28 16:09:16 -0500

    add /build from frontend

---
+++ build/precache-manifest.08e5e7cb6b6c5ad47ac24f4bfa831821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94ce0ecaba661bb2121ffe7d7e2c85b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "342c728a0c84664d0cd9",
+    "url": "/static/css/main.e95cbbf2.chunk.css"
+  },
+  {
+    "revision": "f8781f18c972ae1b4012",
+    "url": "/static/js/2.b3a4dfe2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b3a4dfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342c728a0c84664d0cd9",
+    "url": "/static/js/main.b9f59abd.chunk.js"
+  },
+  {
+    "revision": "042ba4a4b8b8fd878265",
+    "url": "/static/js/runtime-main.2d99711c.js"
+  }
+]);
---
+++ build/precache-manifest.e68cafd966863bdd585251de2bc25551.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc2e51fd6c261dcdc1b6df016e6c833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb6a8a749f12538034b",
+    "url": "/static/css/main.e95cbbf2.chunk.css"
+  },
+  {
+    "revision": "f8781f18c972ae1b4012",
+    "url": "/static/js/2.b3a4dfe2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.b3a4dfe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb6a8a749f12538034b",
+    "url": "/static/js/main.c4c2af80.chunk.js"
+  },
+  {
+    "revision": "042ba4a4b8b8fd878265",
+    "url": "/static/js/runtime-main.2d99711c.js"
+  }
+]);
