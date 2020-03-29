2ff7af46527c24a8c3f29e89c00324b2c7316287
Nghia <nphan@fandm.edu>
2020-03-29 12:20:08 -0500

    implement to work with mongodb atlas

--- index.js
+++ index.js
@@ -1,66 +1,43 @@
+require('dotenv').config() // to use environment variables
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const JournalEntry = require('./models/journalEntry') //import model
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
+	console.log('Method:', request.method)
+	console.log('Path:  ', request.path)
+	console.log('Body:  ', request.body)
+	console.log('---')
+	next()
 }
 app.use(requestLogger)
-
-let journalEntries = [
-	{
-		id: 1,
-		content: "HTML is easy",
-		date: "2019-05-30T17:30:31.098Z",
-		important: true
-	},
-	{
-		id: 2,
-		content: "Browser can execute only Javascript",
-		date: "2019-05-30T18:39:34.091Z",
-		important: false
-	},
-	{
-		id: 3,
-		content: "GET and POST are the most important methods of HTTP protocol",
-		date: "2019-05-30T19:20:14.298Z",
-		important: true
-	}
-]
 
 app.get('/', (req, res) => {
 	res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/journalEntries', (req, res) => {
-	res.json(journalEntries)
+app.get('/api/journalEntries', (request, response) => {
+	JournalEntry.find({}).then(journalEntries => {
+		response.json(journalEntries.map(entry => entry.toJSON()))
+	})
 })
 
-app.get('/api/journalEntries/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const journalEntry = journalEntries.find(entry => entry.id === id)
-
-	if (journalEntry) {
-		response.json(journalEntry)
-	} else {
-		response.status(404).end()
-	}
+app.get('/api/journalEntries/:id', (request, response, next) => {
+	JournalEntry.findById(request.params.id)
+		.then(entry => {
+			if (entry) {
+				response.json(entry.toJSON())
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
-
-const generatedId = () => {
-	const maxId = journalEntries.length > 0
-		? Math.max(...journalEntries.map(n => n.id))
-		: 0
-		return maxId + 1
-}
 
 app.post('/api/journalEntries', (request, response) => {
 	const body = request.body
@@ -71,31 +48,59 @@ app.post('/api/journalEntries', (request, response) => {
 		})
 	}
 
-	const journalEntry = {
+	const journalEntry = new JournalEntry({
 		content: body.content,
 		important: body.important || false,
 		date: new Date(),
-		id: generatedId(),
+	})
+
+	journalEntry.save().then(savedEntry => {
+		response.json(savedEntry.toJSON())
+	})
+})
+
+app.put('/api/journalEntries/:id', (request, response, next) => {
+	const body = request.body
+
+	const journalEntry = {
+		content: body.content,
+    important: body.important,
 	}
 
-	journalEntries = journalEntries.concat(journalEntry)
-
-	response.json(journalEntry)
+	JournalEntry.findByIdAndUpdate(request.params.id, journalEntry, { new: true })
+		.then(updatedEntry => {
+			response.json(updatedEntry.toJSON())
+		})
+		.catch(error => next(error))
 })
 
 app.delete('/api/journalEntries/:id', (request, response) => {
-	const id = Number(request.params.id)
-	journalEntries = journalEntries.filter(entry => entry.id !== id)
-
-	response.status(204).end()
+	JournalEntry.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
 }
+// handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+	console.error('Error:', error.message)
+
+	if (error.name === 'CastError' && error.kind === 'ObjectId') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
+// handler of requests with result to errors
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
